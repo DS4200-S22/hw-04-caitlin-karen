a011e717f8a44f4c638ec1b7bef9189c83e19baa
Caitlin <caitlin.c.chao@gmail.com>
2022-02-10 18:08:38 -0500

    circle2 done, circle3 buggy

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+function border() {
+    let circle = document.getElementById("circle2");
+    circle.classList.add("border");
+    circle.classList.remove("borderless");
+}
+
+function borderless() {
+    let circle = document.getElementById("circle2");
+    circle.classList.add("borderless");
+    circle.classList.remove("border");
+}
+
+function disappear() {
+    let circle = document.getElementById("circle3");
+    circle.classList.toggle("hidden");
+}
